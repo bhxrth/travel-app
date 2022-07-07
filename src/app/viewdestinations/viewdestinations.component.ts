c763aab324a7c4c79d4c2fc5bde92f45fa3754bd
bhxrth <bharathcaptain888@gmail.com>
2022-07-07 21:29:06 +0530

    view destinations created

---
+++ src/app/viewdestinations/viewdestinations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewdestinations',
+  templateUrl: './viewdestinations.component.html',
+  styleUrls: ['./viewdestinations.component.css']
+})
+export class ViewdestinationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
